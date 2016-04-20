5070902dbbd554c9a083044bd593c0771973070e
Nick Miller <nick.miller@onyxpoint.com>
2016-04-20 08:52:15 -0400

    updated bash conditionals to a best practice

--- scripts/simp-bootstrap.sh
+++ scripts/simp-bootstrap.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ "$SIMP_PACKER_run_simp_config" = "yes" ]; then
+if [[ "$SIMP_PACKER_run_simp_config" = "yes" ]]; then
   simp config -a /vagrant/simp_conf.yaml
 fi
 
-if [ "$SIMP_PACKER_run_simp_bootstrap" = "yes" ]; then
+if [[ "$SIMP_PACKER_run_simp_bootstrap" = "yes" ]]; then
   simp bootstrap
 fi
 
