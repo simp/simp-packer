61db8b2d43accfbe6753ed4f10a73453993215a1
Lucas Yamanishi <lucas.yamanishi@onyxpoint.com>
2016-09-19 11:54:14 -0400

    (SIMP-1517) Fix documentation path
    This corrects the post-up documentation to match the files included in
    the simp-testenv.
    
    Change-Id: I4fc15c637fc61699bd5af3c0077c24498f26e4d6

--- scripts/simp-bootstrap.sh
+++ scripts/simp-bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ "$SIMP_PACKER_run_simp_config" = "yes" ]]; then
-  simp config -a /vagrant/simp_conf.yaml
+  simp config -a /vagrant/simp_config_answers.yaml
 fi
 
 if [[ "$SIMP_PACKER_run_simp_bootstrap" = "yes" ]]; then
