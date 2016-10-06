6c414a1f2c77339a412e6215c961a40f5796b6e6
Lucas Yamanishi <lucas.yamanishi@onyxpoint.com>
2016-10-06 19:04:02 -0400

    (SIMP-1517) Don't disable `requiretty` globally
    This patch removes configuration that disabled the sudo option
    `requiretty` globally, favoring per-user configuration.
    
    Change-Id: I0c4aaece156f694d5b4ed0cb36b261901d97e2ed

--- scripts/puppet-bootstrap.sh
+++ scripts/puppet-bootstrap.sh
@@ -29,6 +29,11 @@ class site::vagrant {
     passwd    => false,
     cmnd      => 'ALL'
   }
+  sudo::default_entry { 'vagrant':
+    def_type => 'user',
+    target   => 'vagrant',
+    content  => ['!requiretty'],
+  }
   network::add_eth { 'enp0s3':
     bootproto    => 'none',
     gateway      => '10.0.2.2',
@@ -45,16 +50,6 @@ cat << EOF > /etc/puppet/environments/simp/hieradata/default.yaml
 ---
 classes:
   - 'site::vagrant'
-
-# remove requiretty from defaults so vagrant can provision vm
-simplib::sudoers::default_entry:
-  - 'listpw=all'
-  - 'syslog=authpriv'
-  - '!root_sudo'
-  - '!umask'
-  - 'env_reset'
-  - 'secure_path = /usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin'
-  - 'env_keep = "COLORS DISPLAY HOSTNAME HISTSIZE INPUTRC KDEDIR LS_COLORS MAIL PS1 PS2 QTDIR USERNAME LANG LC_ADDRESS LC_CTYPE LC_COLLATE LC_IDENTIFICATION LC_MEASUREMENT LC_MESSAGES LC_MONETARY LC_NAME LC_NUMERIC LC_PAPER LC_TELEPHONE LC_TIME LC_ALL LANGUAGE LINGUAS _XKB_CHARSET XAUTHORITY"'
 
 simplib::resolv::option_rotate: false
 EOF
