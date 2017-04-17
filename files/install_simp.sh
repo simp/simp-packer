9489159beb3bd4dfea870af8be6a5448baae4f79
Clayton Mentzer <clayton.mentzer@onyxpoint.com>
2017-04-17 18:06:22 -0500

    updates for newest iso, changed to use ec2 user, fixes

--- files/install_simp.sh
+++ files/install_simp.sh
@@ -41,16 +41,16 @@ IP_ADDRESS="${IP_ADDRESS//-/.}"
 echo "New IP_ADDRESS: $IP_ADDRESS"
 
 # 2: Update the simp-config answers file with new hostname
-sed -i "s/ip-172-31-11-170.us-west-2.compute.internal/$HOSTNAME/g" simp_conf.yaml
+sed -i "s/PLACEHOLDER_HOSTNAME/$HOSTNAME/g" simp_conf.yaml
 
 # ... with the new DNS SEARCH
-sed -i "s/us-west-2.compute.internal/$DNS_SEARCH/g" simp_conf.yaml
+sed -i "s/PLACEHOLDER_DNS/$DNS_SEARCH/g" simp_conf.yaml
 
 # ... with the new LDAP DN
-sed -i "s/dc=us-west-2/dc=$LDAP_DN_DC/g" simp_conf.yaml
+sed -i "s/PLACEHOLDER_LDAP/dc=$LDAP_DN_DC/g" simp_conf.yaml
 
 # ... with the new ip_address
-sed -i "s/172.31.11.170/$IP_ADDRESS/g" simp_conf.yaml
+sed -i "s/PLACEHOLDER_IP/$IP_ADDRESS/g" simp_conf.yaml
 
 
 echo "Done replacing hostname etc in simp_conf, running simp config"
@@ -71,20 +71,20 @@ echo "svckill::mode: 'warning'" >> $FQDN_YAML_FILE
 echo "ssh::server::conf::trusted_nets:" >> $FQDN_YAML_FILE
 echo "  - 'ALL'" >> $FQDN_YAML_FILE
 
-# default password hash for the aws user, ideally you'll never log into the user with the password sooooo
+# default password hash for the ec2 user, ideally you'll never log into the user with the password sooooo
 pass_hash='$6$5SnplwrFxmHy4j/v$WgcZV1.W6/wQq1SJh/gnV7E5Tr1iIuJgdCFmhdlzHnCdWR927Q/Q4eKZXtFAVOY7eNRb3e30ezM5xbmP8G7t50'
 
 # add the aws_user
-groupadd -g 1800 aws_user
-useradd -d /var/local/aws_user -g aws_user -m -p $pass_hash -s /bin/bash -u 1800 -K PASS_MAX_DAYS=90 -K PASS_MIN_DAYS=1 -K PASS_WARN_AGE=7 aws_user
-usermod -aG wheel aws_user
-chage -d 0 aws_user
-echo 'AllowUsers aws_user' >> /etc/ssh/sshd_config
+groupadd -g 1800 ec2-user
+useradd -d /var/local/ec2-user -g ec2-user -m -p $pass_hash -s /bin/bash -u 1800 -K PASS_MAX_DAYS=90 -K PASS_MIN_DAYS=1 -K PASS_WARN_AGE=7 ec2-user
+usermod -aG wheel ec2-user
+chage -d 0 ec2-user
+echo 'AllowUsers ec2-user' >> /etc/ssh/sshd_config
 
 # add the aws_user's ssh key
 mkdir /etc/ssh/local_keys
-touch /etc/ssh/local_keys/aws_user
-chmod 644 /etc/ssh/local_keys/aws_user
+touch /etc/ssh/local_keys/ec2-user
+chmod 644 /etc/ssh/local_keys/ec2-user
 
 # get the ssh key from the cloud data
 # Trim the extra S'...' off the hostname 
@@ -96,7 +96,7 @@ SSH_KEY="${SSH_KEY//\'}"
 echo "$SSH_KEY"
 
 # copy public key for user over to new place
-echo "$SSH_KEY" >> /etc/ssh/local_keys/aws_user
+echo "$SSH_KEY" >> /etc/ssh/local_keys/ec2-user
 
 # 4: update site.pp with new stuff:
 # get path to file:
@@ -105,15 +105,15 @@ SITE_MANIFEST="/etc/puppetlabs/code/environments/simp/manifests/site.pp"
 # add pam access rule for a user:
 echo "adding access rules in site.pp"
 cat <<EOT >> $SITE_MANIFEST
-pam::access::rule { 'aws_user':
+pam::access::rule { 'ec2-user':
   permission => '+',
-  users      => ['(aws_user)'],
+  users      => ['(ec2-user)'],
   origins    => ['ALL'],
   order      => 1000
 }
 
-sudo::user_specification { 'aws_user':
-  user_list => ['aws_user'],
+sudo::user_specification { 'ec2-user':
+  user_list => ['ec2-user'],
   passwd    => false,
   host_list => [\$facts['ec2_metadata']['hostname']],
   runas     => 'root',
@@ -121,6 +121,8 @@ sudo::user_specification { 'aws_user':
 }
 EOT
 
+# remove the bootstrap lock that has been put in place by simp config
+rm -f /root/.simp/simp_bootstrap_start_lock
 
 # run simp bootstrap 
 simp bootstrap
---
+++ files/remove_packer_user.sh
@@ -0,0 +1,3 @@
+# after your new user is configured and you are sure that you can log in with that user, run this script to delete the packer user and it's home directory. 
+
+userdel -f -r -Z packer 
