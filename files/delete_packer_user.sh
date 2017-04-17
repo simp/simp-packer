5cff8169cdfb01a81f333b4be248012bea9bb0a2
Clayton Mentzer <clayton.mentzer@onyxpoint.com>
2017-04-17 18:06:22 -0500

    updates for newest iso, changed to use ec2 user, fixes

--- files/delete_packer_user.sh
+++
@@ -1,5 +0,0 @@
-# this script is run after bootstrap and after a new user is configured based on user data, etc
-#
-# 1: remove the /var/local/packer directory
-# 2: remove packer user from /etc/passwd and /etc/sudoers
-#
