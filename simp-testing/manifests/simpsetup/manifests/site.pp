124588babed434dfaa8aef4987c81ac7f7d260d1
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2017-05-29 17:12:29 -0400

    x

--- simp-testing/manifests/simpsetup/manifests/site.pp
+++ simp-testing/manifests/simpsetup/manifests/site.pp
@@ -2,14 +2,14 @@ class simpsetup::site (
   String    $sitedir  = '/etc/puppetlabs/simp/modules/site/manifests'
 ){
 
-  file { "${sitedir}/tftpboot.pp"
+  file { "${sitedir}/tftpboot.pp":
     ensure => file,
     owner  => root,
     group  => puppet,
     mode   => '0640',
     content => template('simpsetup/site/manifests/tftpboot.pp.erb')
   }
-  file { "${sitedir}/workstations.pp"
+  file { "${sitedir}/workstations.pp":
     ensure => file,
     owner  => root,
     group  => puppet,
