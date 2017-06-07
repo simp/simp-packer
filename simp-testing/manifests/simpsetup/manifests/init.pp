e5484e0a654204c8e4e413dfa0653252ecb34796
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2017-06-07 17:20:24 +0000

    document module

--- simp-testing/manifests/simpsetup/manifests/init.pp
+++ simp-testing/manifests/simpsetup/manifests/init.pp
@@ -1,3 +1,29 @@
+# This is intended for use with the simp-packer module.
+#
+# simpsetup si used to configure SIMP for testing/CI
+# It will take the hostname of the puppetserver, ip and
+# mac and generate DNS/DHCP, the Kickstart server, add
+# users to LDAP, set up autosigning, generate server
+# certs.
+#
+# It currently bases everything on the IP, mac and FQDN
+# of the puppet server.   It assumes a class C network and
+# changes the last part of the IP and mac to generate ten
+# servers named server##.<domain name> and ten workstations
+# ws##.<domain name>
+#
+# @param $domain
+# @param $dnsserver The name of the dnsserver. (should be
+#                   puppet server.  It won't work for a remote
+#                   server.)
+# @param $ipaddress The ipaddress of the puppet server
+# @param $relver    The OS release version. (Only works for CentOS)
+# @param $servers   The last octet of the IP for a list of servers to
+#                   to set up data for.
+# @param $clients   The last octet of the IP for a list of workstations
+#                   to set up data.
+# NOTE: The site.pp file is edited in another script in packer.
+#
 class simpsetup(
   String           $domain = $facts['domain'],
   String           $dnsserver = $facts['networking']['fqdn'],
