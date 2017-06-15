db5ec186b7e75fe32419b0fb80b5235d4e33b0d0
Jeanne Greulich <jeanne.greulich@onyxpoint.com>
2017-06-15 21:02:54 +0000

    added audit group and user, fix test params, fix ntp and dns setting in conf.yaml so they are arrays not strings, added more time after reboot for puppet to come up.

--- simp-testing/scripts/simp-bootstrap.sh
+++ simp-testing/scripts/simp-bootstrap.sh
@@ -6,7 +6,12 @@
   echo "**********************"
   simp bootstrap --remove_ssldir --no-track
 # echoing bootstrap log to the log file
+  echo "**********************"
+  echo "Bootstrap Log"
+  echo "**********************"
+
   cat /root/.simp/simp_bootstrap.log*
+  echo "****** End of Bootstrap Log ****************"
 
 #  Have to execute this or the next provisioning scripts
 #  won't be able to ssh and sudo because simp will
