c026ee6acccd8c8cd069d347b80dce4decbd6bfe
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-08-10 21:04:54 +0000

    Grant `vagrant` user password-less sudo

--- scripts/puppet-usersetup.sh
+++ scripts/puppet-usersetup.sh
@@ -22,33 +22,38 @@ class site::vagrant {
     users      => ['vagrant','simp'],
     origins    => ['ALL'],
   }
-  sudo::user_specification { 'vagrant_sudosh':
+
+  # The vagrant user needs Password-less Sudo
+  #
+  #   https://www.vagrantup.com/docs/boxes/base.html#password-less-sudo
+  #
+  sudo::user_specification { 'vagrant_passwordless_sudo':
     user_list => ['vagrant'],
     host_list => ['ALL'],
-    cmnd      => ['/usr/bin/sudosh'],
+    cmnd      => ['ALL'],
     passwd    => false,
   }
-  sudo::user_specification { 'vagrant_ssh':
-    user_list => ['vagrant'],
-    passwd    => false,
-    cmnd      => ['ALL']
-  }
+
   sudo::user_specification { 'simp_sudo':
     user_list => ['simp'],
     host_list => ['ALL'],
     cmnd      => ['ALL'],
     passwd    => false,
   }
+
   sudo::default_entry { 'simp_default_notty':
     content => ['!env_reset, !requiretty'],
     target => 'simp',
     def_type => 'user'
   }
+
   sudo::default_entry { 'vagrant_default_notty':
     content => ['!env_reset, !requiretty'],
     target => 'vagrant',
     def_type => 'user'
   }
+
+  # Make vboxadd* services known to svckill
   service { 'vboxadd': }
   service { 'vboxadd-service': }
 }
