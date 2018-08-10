1966c1867d51b9d79dbe3bb559696c59a0641f24
Chris Tessmer <chris.tessmer@onyxpoint.com>
2018-08-10 19:01:42 +0000

    Clean up code

--- scripts/config/simpsetup.sh
+++ scripts/config/simpsetup.sh
@@ -2,12 +2,12 @@
 # TODO, just make this a module and install it.
 export PATH=$PATH:/opt/puppetlabs/bin
 packerdir="/var/local/simp"
-codedir=`puppet config print environmentpath`
-pupenv=`puppet config print environment`
+codedir=$(puppet config print environmentpath)
+pupenv=$(puppet config print environment)
 puppetmodpath="${codedir}/${pupenv}/modules"
 
-cp -R $packerdir/manifests/simpsetup $puppetmodpath
-chmod -R g+rx $puppetmodpath/simpsetup
-chown -R root:puppet $puppetmodpath/simpsetup
+cp -R "${packerdir}/manifests/simpsetup ${puppetmodpath}"
+chmod -R g+rx "${puppetmodpath}/simpsetup"
+chown -R root:puppet "${puppetmodpath}/simpsetup"
 
 puppet apply -e 'include simpsetup'
