f07fed1aca639ce9d53d825044e01a5c80bdde4d
jeannegreulich <jeanne.greulich@onyxpoint.com>
2019-08-19 14:57:35 -0400

    got tests working but still need to etermine os

--- spec/lib/simp/packer/config/simpjsonfile_writer_spec.rb
+++ spec/lib/simp/packer/config/simpjsonfile_writer_spec.rb
@@ -1,35 +1,37 @@
 require 'simp/packer/config/simpjsonfile_writer'
+require 'pry'
+require 'pry-byebug'
 require 'spec_helper'
 
 
 describe Simp::Packer::Config::SimpjsonfileWriter do
   describe '#render' do
-    let(:settings) {{
-       'settings1' => {
+    let(:settings) {[
+       {
          "firmware" => "bios",
          "disk_encrypt" => "false",
          "fips"         => "fips=0",
          "bootcmd-prefix" => 'simp'
        },
-       'settings2' => {
+       {
          "firmware" => "bios",
          "disk_encrypt" => "true",
          "fips"         => "fips=1",
          "bootcmd-prefix" => 'simp'
        },
-       'settings3' => {
+       {
          "firmware" => "efi",
          "disk_encrypt" => "false",
          "fips"         => "fips=1",
          "bootcmd-prefix" => 'simp'
        },
-       'settings4' => {
+       {
          "firmware" => "bios",
          "disk_encrypt" => "true",
          "fips"         => "fips=0",
          "bootcmd-prefix" => 'simp'
        },
-    }}
+    ]}
 
     let(:basedir) {
        File.expand_path('../../../../../../',__FILE__)
@@ -37,8 +39,10 @@ describe Simp::Packer::Config::SimpjsonfileWriter do
 
     it "returns expected content" do
       settings.each  do | s |
-        rendered_content = described_class.new(s, basedir).render 'simp.json.erb'
-        expected_content = File.read("spec/lib/simp/packer/config/files/simp.json/example.#{s['firmware']}.#{s['disk_encrypt']}.#{s['fips']}.txt")
+         myclass = described_class.new(s, basedir)
+         rendered_content = myclass.render "simp.json/bootcmd/#{s['firmware']}.erb"
+#        rendered_content = described_class.new(s, basedir).render 'simp.json.erb'
+        expected_content = File.read("spec/lib/simp/packer/config/files/simp.json/examples.#{s['firmware']}.#{s['disk_encrypt']}.#{s['fips']}.txt")
         expect(rendered_content).to eq expected_content
       end
     end
